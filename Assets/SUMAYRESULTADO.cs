99a0bd40cdabf912b00dc316fea5d72f710856ee
juligric <49377944@est.ort.edu.ar>
2025-05-13 09:01:16 -0300

    suma y resta

---
+++ Assets/SUMAYRESULTADO.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SUMAYRESULTADO : MonoBehaviour
+{
+    public TMP_InputField inputNum1;
+    public TMP_InputField inputNum2;
+    public TextMeshProUGUI resultadoxt;
+
+    int num1;
+    int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultadoxt.text = "";
+    }
+
+    public void SumaryMostrar()
+    {
+        num1 = int.Parse( inputNum1.text);
+        num2 = int.Parse( inputNum2.text);
+        resultadoxt.text = (num1 + num2) .ToString();
+    }
+}
